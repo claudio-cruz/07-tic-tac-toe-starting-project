c5461109e613b61a3a59181a0b06867269e9447d
Claudio Cruz <claudiocruz1989@gmail.com>
2023-11-14 04:22:07 +0000

    Update Player.jsx

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 
-function Player({name, symbol}) {
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName)
   const [isEditing, setIsEditing] = useState(false);
 
-  const editNameHandler = () => {
-    setIsEditing(true)
+  function editNameHandler() {
+    setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) { setPlayerName(event.target.value) }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type='text' required />;
+    editablePlayerName =
+      <input
+        type='text'
+        required value={playerName}
+        onChange={handleChange}
+      />;
   }
 
   return (
     <li>
       <span className='player'>
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={editNameHandler}>Edit</button>
+      <button onClick={editNameHandler} >
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
     </li>
   );
 }
